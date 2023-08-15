f6baf7c123375a2350bd4d3d01db9eaf382e4fe9
baesku789 <39040522+baesku789@users.noreply.github.com>
2023-08-15 16:54:21 +0900

    refactor: PhoneTextField 따로 분리

--- src/pages/phone-verification.tsx
+++ src/pages/phone-verification.tsx
@@ -1,21 +1,5 @@
-import Button from "@/components/design/Button";
-import TextField from "@/components/design/TextField";
-import { Typography } from "@mui/material";
+import PhoneTextField from "@/phone-verfication/components/PhoneTextField";
 
 export default function PhoneVerficationPage() {
-  return (
-    <div className="p-20 flex flex-col justify-between h-full">
-      <div>
-        <Typography variant="h6">토사장터에 오신걸 환영합니다!</Typography>
-        <Typography variant="h6">시작에 앞서 휴대폰 인증이 필요해요</Typography>
-        <Typography variant="subtitle1">휴대폰 인증 하나로 간편가입</Typography>
-      </div>
-      <div className="flex flex-col gap-12">
-        <Typography variant="subtitle2">입력한 정보를 확인해주세요</Typography>
-        <TextField type="number" label={"휴대폰 번호"} />
-      </div>
-
-      <Button>인증 번호 받기</Button>
-    </div>
-  );
+  return <PhoneTextField />;
 }
