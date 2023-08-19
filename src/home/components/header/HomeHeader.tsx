ed71fab46983e71c6f4f4d1e5c22722f98fbe206
baesku789 <39040522+baesku789@users.noreply.github.com>
2023-08-19 16:53:43 +0900

    feat: HomeLocation

--- src/home/components/header/HomeHeader.tsx
+++ src/home/components/header/HomeHeader.tsx
@@ -1,11 +1,11 @@
 import FindTextField from "@/find/components/FindTextField";
 import FilterList from "./FilterList";
+import HomeLocation from "./HomeLocation";
 
 export default function HomeHeader() {
   return (
     <div className="absolute top-0 w-full flex flex-col gap-12 p-20">
-      <div></div>
-
+      <HomeLocation />
       <FindTextField />
       <FilterList />
     </div>
---
+++ src/home/components/header/HomeLocation.tsx
@@ -0,0 +1,21 @@
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import { Typography } from "@mui/material";
+import { useState } from "react";
+
+export default function HomeLocation() {
+  const [click, setClick] = useState(false);
+
+  const onClick = () => {
+    setClick((prev) => !prev);
+  };
+
+  return (
+    <div className="flex" onClick={onClick}>
+      <Typography variant="h6">현장설정</Typography>
+      <PlayArrowIcon
+        className="transition-all w-content h-content mt-3"
+        style={{ transform: click ? "rotate(90deg)" : "rotate(270deg)" }}
+      />
+    </div>
+  );
+}
