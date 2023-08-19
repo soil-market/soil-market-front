8fdcbd3f8218acc3a249e4cde4a31d8f6dbaaed3
baesku789 <39040522+baesku789@users.noreply.github.com>
2023-08-19 10:39:23 +0900

    feat: HomePage, HomeHeader

---
+++ src/home/components/HomeHeader.tsx
@@ -0,0 +1,9 @@
+export default function HomeHeader() {
+  return (
+    <div className="absolute top-0 w-full flex flex-col gap-12">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
