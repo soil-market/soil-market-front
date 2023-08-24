b5f9bdf9a3a4cb7fab4cf599805b0f69f4c4184f
baesku789 <39040522+baesku789@users.noreply.github.com>
2023-08-24 21:44:52 +0900

    refactor: firebase apiKye, appId 환경 변수 처리

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -10,12 +10,12 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqKYR1vc1VS-WikvPEeG6pMkR38JGDqd0",
+  apiKey: process.env.NEXT_PIBLIC_API_KEY,
   authDomain: "soilmarket.firebaseapp.com",
   projectId: "soilmarket",
   storageBucket: "soilmarket.appspot.com",
   messagingSenderId: "602882915639",
-  appId: "1:602882915639:web:34695680258be343a1aeb2",
+  appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: "G-1V6FFD2ZTN",
 };
 
