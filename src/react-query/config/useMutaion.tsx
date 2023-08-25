8b018cacb790b084e681dc68f5ddf3ffb4541d1b
baesku789 <39040522+baesku789@users.noreply.github.com>
2023-08-25 23:17:01 +0900

    feat: custom useMutaion (기본 에러 처리 아직 안함)

---
+++ src/react-query/config/useMutaion.tsx
@@ -0,0 +1,26 @@
+import {
+  MutationFunction,
+  MutationKey,
+  UseMutationOptions,
+  useMutation,
+} from "@tanstack/react-query";
+
+export { useMutation as baseUseMutaion } from "@tanstack/react-query";
+
+export default function useMutaion(
+  queryKey: MutationKey,
+  queryFn: MutationFunction<unknown, void> | undefined,
+  options:
+    | Omit<
+        UseMutationOptions<unknown, unknown, void, unknown>,
+        "mutationKey" | "mutationFn"
+      >
+    | undefined
+) {
+  return useMutation(queryKey, queryFn, {
+    onError: () => {
+      // 기본 에러 처리
+    },
+    ...options,
+  });
+}
