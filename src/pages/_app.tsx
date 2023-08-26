f119c0e825a3a6c83935b8e654500e4b8bee9e6b
baesku789 <39040522+baesku789@users.noreply.github.com>
2023-08-27 00:00:42 +0900

    refactor: 필요 없는 css 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import LoadingContainer from "@/components/container/LoadingContainer";
 import Layout from "@/components/layout/Layout";
-import useIosVh from "@/hooks/useIosVh";
 import "@/styles/globals.css";
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -9,8 +8,6 @@ import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
-
-  useIosVh();
 
   return (
     <QueryClientProvider client={queryClient}>
